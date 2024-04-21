64ed1079506a5ae0f1fdddef8c5a0bdd45961b9b
=?UTF-8?q?Andy=20=7C=20=E3=82=A2=E3=83=B3=E3=83=87=E3=82=A3?= <ayamgorengmanis87@gmail.com>
2024-04-21 18:42:06 +0200

    sm8450-common: Compile HWUI for performance
    * Activates a few optimizing compiler flags for HWUI.
    * -fno-omit-frame-pointer, -marm, -mapcs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,6 +200,9 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := taro
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # PowerShare
 $(call soong_config_set, XIAOMI_POWERSHARE, WIRELESS_TX_ENABLE_PATH, /sys/class/qcom-battery/reverse_chg_mode)
 
