ec65d7d05d28c285fddde1418e35f9890d6e594f
finish0314 <finish21190@gmail.com>
2024-09-15 13:40:35 +0800

    sm8450-common: parts: Checkout thermal profiles to xiaomi-sm8250 implementation

--- parts/src/org/lineageos/settings/thermal/ThermalActivity.java
+++ parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2022 The LineageOS Project
+ * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,20 +14,32 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.thermal;
+ package org.lineageos.settings.thermal;
 
-import android.os.Bundle;
-
-import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-
-public class ThermalActivity extends CollapsingToolbarBaseActivity {
-    private static final String TAG_THERMAL = "thermal";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+ import android.os.Bundle;
+ import android.view.MenuItem;
+ 
+ import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+ 
+ public class ThermalActivity extends CollapsingToolbarBaseActivity {
+ 
+     private static final String TAG_THERMAL = "thermal";
+     private static final String THERMAL_SCONFIG = "/sys/class/thermal/thermal_message/sconfig";
+ 
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+ 
          getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
-                new ThermalSettingsFragment(), TAG_THERMAL).commit();
-    }
-}
+                 new ThermalSettingsFragment(), TAG_THERMAL).commit();
+     }
+ 
+     @Override
+     public boolean onOptionsItemSelected(MenuItem item) {
+         if (item.getItemId() == android.R.id.home) {
+             onBackPressed();
+             return true;
+         }
+         return false;
+     }
+ }
