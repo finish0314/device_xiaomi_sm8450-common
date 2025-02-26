e97d3787897139f4c2af1cd9bb011148d76c0de5
finish0314 <finish21190@gmail.com>
2025-02-26 19:29:37 +0800

    fixup! sm8450-common: fix a typo

--- common.mk
+++ common.mk
@@ -232,7 +232,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     init.qti.media.rc \
-    init.qti.media.sh \
+    init.qti.media.sh
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
