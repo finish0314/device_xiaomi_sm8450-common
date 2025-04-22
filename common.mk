2da1ab28de346c690f8a29b1ab38da014670e739
finish0314 <finish21190@gmail.com>
2025-04-22 23:21:29 +0800

    sm8450-common: Add Baidu network location support for China region

--- common.mk
+++ common.mk
@@ -187,6 +187,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
+# Gnss
+PRODUCT_PACKAGES += \
+    BaiduNetworkLocation
+
 # Graphics
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
@@ -273,6 +277,7 @@ PRODUCT_PACKAGES += \
     WifiResCommon
 
 PRODUCT_PACKAGES += \
+    FrameworkResCN \
     DialerResXiaomi \
     FrameworksResTarget \
     FrameworksResXiaomi \
