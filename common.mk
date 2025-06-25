7a2aa4bc4abb1eb9110e0d95ce261b4b617743b0
finish0314 <finish21190@gmail.com>
2025-06-26 01:36:44 +0800

    sm8450-common: Drop unused overlay

--- common.mk
+++ common.mk
@@ -308,7 +308,6 @@ PRODUCT_PACKAGES += \
     DialerResXiaomi \
     FrameworksResTarget \
     FrameworksResXiaomi \
-    LineageResXiaomi \
     SettingsProviderResXiaomi \
     SettingsResXiaomi \
     WifiResTarget \
